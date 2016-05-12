c1ec5896f596a240052abd57d293711b37d68851
srabeeh <srabeeh@larci.com>
2016-05-12 17:05:03 -0600

    Add linked lists tests

---
+++ AlgoDataStructures/LinkedListTests/DoubleLists/Clear.cs
@@ -0,0 +1,55 @@
+﻿using NUnit.Framework;
+using DoubleLinkedList;
+
+namespace LinkedListTests.DoubleLists
+{
+    [TestFixture]
+    public class Clear
+    {
+        [Test]
+        public void Clear_Empty()
+        {
+            LinkedList<int> list = new LinkedList<int>();
+
+            Assert.IsNull(list.Head);
+            Assert.IsNull(list.Tail);
+            Assert.AreEqual(0, list.Count);
+
+            list.Clear();
+
+            Assert.IsNull(list.Head);
+            Assert.IsNull(list.Tail);
+            Assert.AreEqual(0, list.Count);
+        }
+
+        [Test, TestCaseSource("_clear_Success_Cases")]
+        public void Clear_VariousItems(int[] testCase)
+        {
+            LinkedList<int> list = new LinkedList<int>();
+            foreach (int value in testCase)
+            {
+                list.AddLast(new LinkedListNode<int>(value));
+            }
+
+            Assert.IsNotNull(list.Head);
+            Assert.IsNotNull(list.Tail);
+            Assert.AreEqual(testCase.Length, list.Count);
+
+            list.Clear();
+
+            Assert.IsNull(list.Head);
+            Assert.IsNull(list.Tail);
+            Assert.AreEqual(0, list.Count);
+        }
+
+        static object[] _clear_Success_Cases =
+                     {
+                            new int[] { 0 }, 
+                            new int[] { 0, 1 }, 
+                            new int[] { 0, 1, 2 }, 
+                            new int[] { 0, 1, 2, 3 }, 
+                     };
+
+
+    }
+}
